0a7a3025a1f683f428dcbde023679366ba62df5f
mizan71 <56040932+mizan71@users.noreply.github.com>
2020-07-15 17:49:23 +0600

    first commit

---
+++ frequency.py
@@ -0,0 +1,23 @@
+try:
+    with open(r"C:\Users\lenovo\Desktop\final project.txt", "w") as file:
+        sentence = "There is something new"
+        frequency = {}
+        for words in sentence:
+            if words != " ":
+                if words in frequency:
+                    frequency[words] += 1
+                else:
+                    frequency[words] = 1
+        sorts = sorted(frequency.items(),
+                       key=lambda row: row[1],
+                       reverse=True)
+        mapping = map(lambda sort: "{} : {}".format(sort[0], sort[1]), sorts)
+        file.write(f"Hi there! Welcome to this file."
+                   f"\nHere is the sentence--> '{sentence}' \n"
+                   f"And The number of frequency is given below:\n\n")
+        for x in mapping:
+            print(x)
+            file.write("  {}\n".format(x))
+
+except (ValueError, TypeError):
+    print("You've inputted a wrong value")
